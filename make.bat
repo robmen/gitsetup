1cdc5fa16f02e483713da1f2f5b324692a7b843d
Rob Mensching <rob@robmensching.com>
2013-09-11 16:07:03 -0700

    Add git-flow to install.

--- make.bat
+++ make.bat
@@ -7,8 +7,8 @@ pushd %_P%
 if /i "%1"=="bundle" goto Bundle
 
 rmdir /s/q build
-candle src\GitProduct.wxs src\GitComponents.wxs -ext WixDependencyExtension -o build\obj\
-light build\obj\GitProduct.wixobj build\obj\GitComponents.wixobj -ext WixDependencyExtension -o build\msi\git.msi -b src\portable -sval
+candle src\GitProduct.wxs src\GitComponents.wxs src\GitFlowComponents.wxs -ext WixDependencyExtension -o build\obj\
+light build\obj\GitProduct.wixobj build\obj\GitComponents.wixobj build\obj\GitFlowComponents.wixobj -ext WixDependencyExtension -o build\msi\git.msi -b src\portable -b lib -sval
 
 :Bundle
 candle src\GitBundle.wxs -o build\obj\ -ext WixBalExtension -ext WixDependencyExtension
