c5163b0e3f2a09a394e4480652a58e5b6fc90b1b
Rob Mensching <rob@robmensching.com>
2013-09-11 13:25:48 -0700

    Use explicit ProviderKey.

--- make.bat
+++ make.bat
@@ -4,12 +4,15 @@ setlocal
 set _P=%~dp0
 pushd %_P%
 
-rmdir /s/q build
-candle src\GitProduct.wxs src\GitComponents.wxs -o build\obj\
-light build\obj\GitProduct.wixobj build\obj\GitComponents.wixobj -o build\msi\git.msi -b src\portable -sval
+if /i "%1"=="bundle" goto Bundle
 
-candle src\GitBundle.wxs -o build\obj\ -ext WixBalExtension
-light build\obj\GitBundle.wixobj -o build\gitsetup.exe -b build\msi -b portable -b src -ext WixBalExtension
+rmdir /s/q build
+candle src\GitProduct.wxs src\GitComponents.wxs -ext WixDependencyExtension -o build\obj\
+light build\obj\GitProduct.wixobj build\obj\GitComponents.wixobj -ext WixDependencyExtension -o build\msi\git.msi -b src\portable -sval
+
+:Bundle
+candle src\GitBundle.wxs -o build\obj\ -ext WixBalExtension -ext WixDependencyExtension
+light build\obj\GitBundle.wixobj -o build\gitsetup.exe -b build\msi -b portable -b src -ext WixBalExtension -ext WixDependencyExtension
 
 popd
 endlocal
