92333df0a230bb22951f10dd3fdfbf4bdc139416
Rob Mensching <rob@robmensching.com>
2015-04-08 09:39:34 -0700

    Use local copy of WiX toolset.

--- harvest.cmd
+++ harvest.cmd
@@ -4,7 +4,7 @@ setlocal
 set _P=%~dp0
 pushd %_P%
 
-heat dir src\portable -ag -srd -sreg -scom -cg GitComponents -dr INSTALLFOLDER -o src\GitComponents.wxs
+tools\wix\heat dir src\portable -ag -srd -sreg -scom -cg GitComponents -dr INSTALLFOLDER -o src\GitComponents.wxs
 
 popd
 endlocal
--- make.bat
+++ make.bat
@@ -7,12 +7,12 @@ pushd %_P%
 if /i "%1"=="bundle" goto Bundle
 
 rmdir /s/q build
-candle src\GitProduct.wxs src\GitComponents.wxs src\GitCredStoreComponents.wxs src\GitFlowComponents.wxs src\GitPoshComponents.wxs -ext WixDependencyExtension -ext WixUtilExtension -o build\obj\
-light build\obj\GitProduct.wixobj build\obj\GitComponents.wixobj build\obj\GitCredStoreComponents.wixobj build\obj\GitFlowComponents.wixobj build\obj\GitPoshComponents.wixobj -ext WixDependencyExtension -ext WixUtilExtension -o build\msi\git.msi -b src\portable -b lib -sval
+tools\wix\candle src\GitProduct.wxs src\GitComponents.wxs src\GitCredStoreComponents.wxs src\GitFlowComponents.wxs src\GitPoshComponents.wxs -ext WixDependencyExtension -ext WixUtilExtension -o build\obj\
+tools\wix\light build\obj\GitProduct.wixobj build\obj\GitComponents.wixobj build\obj\GitCredStoreComponents.wixobj build\obj\GitFlowComponents.wixobj build\obj\GitPoshComponents.wixobj -ext WixDependencyExtension -ext WixUtilExtension -o build\msi\git.msi -b src\portable -b lib -sval
 
 :Bundle
-candle src\GitBundle.wxs -o build\obj\ -ext WixBalExtension -ext WixDependencyExtension
-light build\obj\GitBundle.wixobj -o build\gitsetup.exe -b build\msi -b portable -b src -ext WixBalExtension -ext WixDependencyExtension
+tools\wix\candle src\GitBundle.wxs -o build\obj\ -ext WixBalExtension -ext WixDependencyExtension
+tools\wix\light build\obj\GitBundle.wixobj -o build\gitsetup.exe -b build\msi -b portable -b src -ext WixBalExtension -ext WixDependencyExtension
 
 popd
 endlocal
