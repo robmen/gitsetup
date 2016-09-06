0793bb928f19fcf950dea516ff2e061a299c7865
Rob Mensching <rob@robmensching.com>
2016-09-05 23:28:51 -0700

    Update to Git v2.10.0
    Disable git-credential-winstore.exe when .NET v4.5.1 or later is present
    since Git-Credential-Manager-for-Windows is now integrated with Git for
    Windows.

--- make.bat
+++ make.bat
@@ -7,8 +7,8 @@ pushd %_P%
 if /i "%1"=="bundle" goto Bundle
 
 rmdir /s/q build
-tools\wix\candle src\GitProduct.wxs src\GitComponents.wxs src\GitCredStoreComponents.wxs src\GitPoshComponents.wxs -ext WixDependencyExtension -ext WixUtilExtension -o build\obj\
-tools\wix\light build\obj\GitProduct.wixobj build\obj\GitComponents.wixobj build\obj\GitCredStoreComponents.wixobj build\obj\GitPoshComponents.wixobj -ext WixDependencyExtension -ext WixUtilExtension -o build\msi\git.msi -b src\portable -b lib -sval
+tools\wix\candle src\GitProduct.wxs src\GitComponents.wxs src\GitCredStoreComponents.wxs src\GitPoshComponents.wxs -ext WixDependencyExtension -ext WixNetfxExtension -ext WixUtilExtension -o build\obj\
+tools\wix\light build\obj\GitProduct.wixobj build\obj\GitComponents.wixobj build\obj\GitCredStoreComponents.wixobj build\obj\GitPoshComponents.wixobj -ext WixDependencyExtension -ext WixNetfxExtension -ext WixUtilExtension -o build\msi\git.msi -b src\portable -b lib -sval
 
 :Bundle
 tools\wix\candle src\GitBundle.wxs -o build\obj\ -ext WixBalExtension -ext WixDependencyExtension
